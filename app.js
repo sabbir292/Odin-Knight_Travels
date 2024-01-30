a70720170f0922a2d48174a4683dd1bad316aa38
sabbir292 <113119826+sabbir292@users.noreply.github.com>
2024-01-30 09:20:41 +0530

    Created the getGraphList, returns a adjacencey list will possible moves of a move

--- app.js
+++ app.js
@@ -42,3 +42,135 @@ const getPossibleMoves = (initial, movesArr = []) => {
     return movesArr
 }
 console.log(getPossibleMoves([0, 0]))
+
+
+//getGraphList returns a adjacency list, for each move it contains the array of it's possible moves.
+const getGraphList = (board, graphList = []) => {
+    board.map(item => {
+        graphList.push(getPossibleMoves(item))
+    })
+    return graphList
+}
+console.log(getGraphList(board))
+
+
+const isEqual = (arr1, arr2) => {
+    if (arr1.length !== arr2.length) {
+        return false
+    }
+
+    for (let i = 0; i < arr1.length; i++) {
+        if (arr1[i] !== arr2[i] || arr1[i + 1] !== arr2[i + 1]) {
+            return false
+        }
+        return true
+    }
+}
+const getIndex = (target) => {
+    for (let i = 0; i < board.length; i++) {
+        let index = isEqual(target, board[i])
+        if (index) return i
+        else continue
+    }
+    return false
+}
+console.log(getIndex([0, 1]))
+
+const hasVisited = (node, data) => {
+    for (let i = 0; i < data.length; i++) {
+        const [a, b] = node
+        const [x, y] = data[i]
+
+        if (a === x && b === y) return true
+    }
+    return false
+}
+const knightMoves = (origin, target, graph = getGraphList(board)) => {
+    let [x, y] = target
+
+    const que = [{ currentPosition: origin, path: [origin] }]
+    const visitedNodes = []
+
+    while (que.length > 0) {
+        const { currentPosition, path } = que.shift()
+        visitedNodes.push(currentPosition)
+        let [a, b] = currentPosition
+
+        if (a === x && b === y) return path
+
+        else {
+            const idx = getIndex(currentPosition)
+            if(!idx) return "Invalid Origin"
+            else{
+                const moves = graph[idx]
+
+                moves.map(move => {
+                    if (!hasVisited(move, visitedNodes)) {
+                        que.push({
+                            currentPosition: move,
+                            path: [...path, move]
+                        })
+                    }
+                })
+            }
+        }
+    }
+    return false
+}
+
+console.log(knightMoves([1,7], [7, 5]))
+
+
+
+
+
+const knightMoves = (source, target) => {
+
+    const moves = [
+        [2, 1],
+        [1, 2],
+        [-1, 2],
+        [-2, 1],
+        [-2, -1],
+        [-1, -2],
+        [1, -2],
+        [2, -1],
+      ];
+
+    
+    console.log([2, 1].toString())
+    const isValidMoves = (x, y) => {
+        if (x < 0 || x > 7 || y > 7 || y < 0) return false
+        return true
+    }
+    console.log(isValidMoves(1, 7))
+
+    const que = [{
+        currentPostition: source,
+        path: [source]
+    }]
+
+    while (que.length > 0) {
+        const { currentPostition, path } = que.shift()
+
+        if (currentPostition.toString() === target.toString()) {
+            return path
+        }
+        else {
+            for (const move of moves) {
+                const newX = currentPostition[0] + move[0]
+                const newY = currentPostition[1] + move[1]
+
+                if (isValidMoves(newX, newY)) {
+                    que.push({
+                        currentPostition: [newX, newY],
+                        path: [...path, [newX, newY]]
+                    })
+                }
+            }
+        }
+    }
+    return false
+}
+
+console.log(knightMoves([0,7], [1,5]))
